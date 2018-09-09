7711171c4dac3cf923869ad5e0bdf1bb307ee50a
gentle-ohige <>
2018-09-09 22:16:45 +0900

    [add] base project

---
+++ react-swift-test/react-swift-testTests/react_swift_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  react_swift_testTests.swift
+//  react-swift-testTests
+//
+//  Created by ShichoChin on 2018/09/09.
+//  Copyright © 2018 Chin ShiCho. All rights reserved.
+//
+
+import XCTest
+@testable import react_swift_test
+
+class react_swift_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
